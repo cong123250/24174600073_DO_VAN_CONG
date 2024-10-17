ef6e9fd80bbbd9794bc7aa30539decbb5ea0c009
cong123250 <dvcong.24174600073@sv.uneti.edu.vn>
2024-10-17 09:32:21 +0700

    abc

---
+++ hello.py
@@ -0,0 +1,8 @@
+print_hello_star():
+    print(* README.md hello.py README.md hello.py README.md hello.py README.md hello.py *****)
+    print(* README.md hello.py README.md hello.py README.md hello.py README.md hello.py README.md hello.py **)
+    print(****** README.md hello.py README.md hello.py README.md hello.py README.md hello.py **)
+    print(* README.md hello.py README.md hello.py README.md hello.py README.md hello.py README.md hello.py **)
+    print(* README.md hello.py README.md hello.py README.md hello.py README.md hello.py *****)
+
+print_hello_star()
